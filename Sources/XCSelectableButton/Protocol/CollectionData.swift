25ffdb75500ff57c6fc6dc2b1fdb0102ec38b924
xchelcd <xchel.co@gmail.com>
2023-10-15 19:45:06 -0600

    added collection data to handle multiples data types inside the same view controller

---
+++ Sources/XCSelectableButton/Protocol/CollectionData.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Xchel Carranza on 15/10/23.
+//
+
+import Foundation
+
+protocol CollectionData: SelectableNameProtocol {
+    func toString() -> String
+}
